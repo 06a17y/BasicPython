41b5df67b036376b3875b229b8036962daeada45
Ally Hsu <06a17y@gmail.com>
2023-06-02 00:26:55 +0800

    class and object

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 # Class、Object
 
-
 class Person:
-    def __init__(self, name, age, country):
+    def __init__(self, name, gender,age, country):
         self.name = name
+        self.gender = gender
         self.age = age
         self.country = country
     
-Student = Person("Amy", 20, "Japan")
+    def bio_gender(self):
+        if self.gender == "female":
+            return "bio-female"
+        else:
+            return "bio-male"
+
+    def add(self, num1, num2):
+        return num1 + num2
+
+Student = Person("Amy", "female", 20, "Japan") # Student object has three attributes
+
+print(Student.name) # Amy
+print(Student.bio_gender()) # bio-female
+print(Student.add(5,6)) # 11
